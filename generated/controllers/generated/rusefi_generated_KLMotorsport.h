ca08aec1697ab62d4e5ff9b32644dc94b7506403
GitHub git update Action <action@github.com>
2024-12-31 18:13:18 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KLMotorsport.h
+++ generated/controllers/generated/rusefi_generated_KLMotorsport.h
@@ -1333,7 +1333,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1331080704
+#define SIGNATURE_HASH 38131352
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1683,7 +1683,7 @@
 #define ts_show_cylinder_bank true
 #define ts_show_debug_mode false
 #define ts_show_ego2 true
-#define ts_show_egt false
+#define ts_show_egt true
 #define ts_show_etb true
 #define ts_show_etb_bias_curve true
 #define ts_show_etb_extra_monitoring 
@@ -1719,7 +1719,7 @@
 #define ts_show_gdi false
 #define ts_show_gdi_low_level true
 #define ts_show_globalTriggerAngleOffset true
-#define ts_show_hardware_simulator false
+#define ts_show_hardware_simulator true
 #define ts_show_hbridge_function true
 #define ts_show_hd true
 #define ts_show_hip9011 false
@@ -1776,7 +1776,7 @@
 #define ts_show_strokes true
 #define ts_show_sync_cam true
 #define ts_show_tachometer true
-#define ts_show_tcu false
+#define ts_show_tcu true
 #define ts_show_throttle_switch true
 #define ts_show_tle8888 false
 #define ts_show_top_level_can_menu true
@@ -1801,7 +1801,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.12.31.KLMotorsport.1331080704"
+#define TS_SIGNATURE "rusEFI main.2024.12.31.KLMotorsport.38131352"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_KLMotorsport.h
+++ generated/controllers/generated/signature_KLMotorsport.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1331080704
-#define TS_SIGNATURE "rusEFI main.2024.12.31.KLMotorsport.1331080704"
+#define SIGNATURE_HASH 38131352
+#define TS_SIGNATURE "rusEFI main.2024.12.31.KLMotorsport.38131352"
