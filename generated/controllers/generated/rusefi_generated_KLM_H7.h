93831254bc84412430c8e5f977889cca71d742ba
GitHub git update Action <action@github.com>
2025-03-04 00:54:36 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KLM_H7.h
+++ generated/controllers/generated/rusefi_generated_KLM_H7.h
@@ -1877,7 +1877,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.03.03.KLM_H7.4019344903"
+#define TS_SIGNATURE "rusEFI main.2025.03.04.KLM_H7.4019344903"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
