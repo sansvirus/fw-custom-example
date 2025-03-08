f31867ae33b435a9371c65b6ef28a9741399c9e8
GitHub git update Action <action@github.com>
2025-03-08 00:43:49 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KLM_H7.h
+++ generated/controllers/generated/signature_KLM_H7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 809130477
-#define TS_SIGNATURE "rusEFI main.2025.03.07.KLM_H7.809130477"
+#define SIGNATURE_HASH 773903217
+#define TS_SIGNATURE "rusEFI main.2025.03.08.KLM_H7.773903217"
